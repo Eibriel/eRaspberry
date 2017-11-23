fa97e48f9b1c6ad1e56a73f2b5f43ac7743bb535
Eibriel <eibriel@eibriel.com>
2017-11-23 15:49:28 -0300

    Adding warning

--- door.py
+++ door.py
@@ -1,13 +1,17 @@
 import time
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except:
+    print ("Run with SUDO!")
 
-# Open door
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.output(23, 0) #OFF
 
 while True:
     if 0:
+        # Open door
         GPIO.output(23, 1) #ON
         time.sleep(5)
         GPIO.output(23, 0) #OFF
